ed9b712aa576530f45a9a817ce645abe57fa79a1
Joel Weiser <joel.weiser@oicr.on.ca>
2020-01-20 14:47:21 -0500

    refactors ProtegeExporterIT.java to use configured test values

--- src/test/java/org/reactome/release/downloaddirectory/ProtegeExporterIT.java
+++ src/test/java/org/reactome/release/downloaddirectory/ProtegeExporterIT.java
@@ -3,19 +3,22 @@ package org.reactome.release.downloaddirectory;
 import static org.junit.Assert.assertTrue;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
+import java.util.stream.Collectors;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.FileUtils;
 import org.gk.persistence.MySQLAdaptor;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,61 +27,61 @@ import org.junit.Test;
 public class ProtegeExporterIT
 {
 	private static final String RELEASE_NUM = "70";
+	private static final String pathToJavaRoot = ".";
+
 	private static String host;
 	private static String name;
 	private static String user;
 	private static String password;
-	// Set pathToJavaRoot from the command line as: -DpathToJavaRoot="/path/to/data-release-pipeline/downloadDirectory/"
-	static final String pathToJavaRoot = System.getProperty("pathToJavaRoot");
-	// Set clean up from the command line as: -Dcleanup=true
-	static final boolean cleanupAfterTest = Boolean.valueOf(System.getProperty("cleanup"));
+	private static String releaseDir;
+	private static List<String> perlLibPaths;
 
 	@Before
-	public void setup() throws FileNotFoundException, IOException, Exception
+	public void setup() throws IOException
 	{
-		if (pathToJavaRoot == null || pathToJavaRoot.trim().isEmpty())
-		{
-			throw new Exception("pathToJavaRoot cannot be empty! Set the value when running Java as: -DpathToJavaRoot=\"/path/to/data-release-pipeline/downloadDirectory/\"");
-		}
-
 		System.out.println("pathToJavaRoot is: " + pathToJavaRoot);
 
 		Properties props = new Properties();
-		try(FileInputStream fis = new FileInputStream("src/test/resources/db.properties"))
-		{
-			props.load(fis);
-			ProtegeExporterIT.host = props.getProperty("db.host");
-			ProtegeExporterIT.name = props.getProperty("db.name");
-			ProtegeExporterIT.user = props.getProperty("db.user");
-			ProtegeExporterIT.password = props.getProperty("db.password");
-			String release = RELEASE_NUM;
-			Files.createDirectories(Paths.get(release+"/"));
+		try(FileInputStream integrationTestProperties = new FileInputStream("src/test/resources/it.properties")) {
+			props.load(integrationTestProperties);
 		}
+
+		ProtegeExporterIT.host = props.getProperty("db.host");
+		ProtegeExporterIT.name = props.getProperty("db.name");
+		ProtegeExporterIT.user = props.getProperty("db.user");
+		ProtegeExporterIT.password = props.getProperty("db.password");
+		ProtegeExporterIT.releaseDir = props.getProperty("release.dir");
+		ProtegeExporterIT.perlLibPaths = getPerlLibPathArguments(props.getProperty("perllib.paths"));
+
+		Files.createDirectories(Paths.get(RELEASE_NUM));
 	}
 
 	@Test
 	public void testProtegeExporterIT() throws SQLException, IOException
 	{
-		MySQLAdaptor adaptor = new MySQLAdaptor(ProtegeExporterIT.host, ProtegeExporterIT.name, ProtegeExporterIT.user, ProtegeExporterIT.password);
+		MySQLAdaptor adaptor = new MySQLAdaptor(
+			ProtegeExporterIT.host, ProtegeExporterIT.name, ProtegeExporterIT.user, ProtegeExporterIT.password
+		);
 
 		ProtegeExporter testExporter = new ProtegeExporter();
 
-		testExporter.setReleaseDirectory("/home/sshorser/workspaces/reactome/Release");
-		testExporter.setPathToWrapperScript(pathToJavaRoot + "src/main/resources/");
-		testExporter.setExtraIncludes(Arrays.asList("-I/home/ubuntu/perl5/lib/perl5/","-I/home/sshorser/perl5/lib/perl5/"));
+		testExporter.setReleaseDirectory(releaseDir);
+		testExporter.setPathToWrapperScript(Paths.get(pathToJavaRoot, "src", "main", "resources").toString());
+		testExporter.setExtraIncludes(perlLibPaths);
 		testExporter.setParallelism(4);
-		Set<Long> pathwayIds = new HashSet<>(Arrays.asList(1670466L,8963743L,870392L,1500931L,5205647L));
+		Set<Long> pathwayIds = new HashSet<>(Arrays.asList(1670466L, 8963743L, 870392L, 1500931L, 5205647L));
 		testExporter.setPathwayIdsToProcess(pathwayIds);
 		testExporter.setDownloadDirectory(RELEASE_NUM);
-		testExporter.setSpeciesToProcess(new HashSet<>(Arrays.asList("Mycobacterium tuberculosis")));
+		testExporter.setSpeciesToProcess(new HashSet<>(Collections.singletonList("Mycobacterium tuberculosis")));
 		testExporter.execute(adaptor);
-		final String pathToFinalTar = pathToJavaRoot + RELEASE_NUM + "/protege_files.tar";
+
+		final String pathToFinalTar = Paths.get(pathToJavaRoot, RELEASE_NUM, "protege_files.tar").toString();
 		assertTrue(Files.exists(Paths.get(pathToFinalTar)));
 		assertTrue(Files.size(Paths.get(pathToFinalTar)) > 0);
 
 		// Now, let's see if the tar contents are valid.
 		try(InputStream inStream = new FileInputStream(pathToFinalTar);
-			TarArchiveInputStream tains = new TarArchiveInputStream(inStream);)
+			TarArchiveInputStream tains = new TarArchiveInputStream(inStream))
 		{
 			boolean done = false;
 			boolean tarContainsPathwayID = false;
@@ -104,11 +107,28 @@ public class ProtegeExporterIT
 			}
 			assertTrue(tarContainsPathwayID);
 		}
-		if (cleanupAfterTest)
+
+		if (cleanupAfterTest())
 		{
 			System.out.println("You specified \"cleanup\", so " + pathToFinalTar + " will now be removed.");
-			Files.delete(Paths.get(pathToFinalTar));
+			FileUtils.deleteDirectory(Paths.get(RELEASE_NUM).toFile());
 		}
 	}
 
+	// Set clean up from the command line as: -Dcleanup=true (cleanup is false by default)
+	private boolean cleanupAfterTest()
+	{
+		String cleanupAfterTest = System.getProperty("cleanup");
+
+		return cleanupAfterTest != null && Boolean.parseBoolean(cleanupAfterTest);
+	}
+
+	// Takes a comma delimited String of paths to library directories for Perl, appends the "-I" flag, and returns
+	// the paths as a list of Strings
+	private List<String> getPerlLibPathArguments(String perlLibPaths)
+	{
+		return Arrays.stream(perlLibPaths.split(","))
+			.map(path -> "-I" + path)
+			.collect(Collectors.toList());
+	}
 }
