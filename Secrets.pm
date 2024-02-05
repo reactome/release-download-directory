559e1da6fa0f5c532ac4b6166add19a4a3cb6693
Adam Wright <adam.wright@wormbase.org>
2024-02-05 17:21:21 -0500

    Create Secrets.pm

---
+++ Secrets.pm
@@ -0,0 +1,19 @@
+package GKB::Secrets;
+# Not be commited on git hub (see .gitignore)
+
+use strict;
+use vars qw/
+$GK_DB_HOST
+$GK_DB_USER
+$GK_DB_PASS
+$GK_DB_NAME
+$GK_IDB_NAME
+$GK_DB_PORT
+/;
+
+$GK_DB_HOST  = 'localhost';
+$GK_DB_USER  = '*db_user*';
+$GK_DB_PASS  = '*db_pass*';
+$GK_DB_NAME  = 'current';
+$GK_IDB_NAME = 'stable_identifiers';
+$GK_DB_PORT  = 3306;
